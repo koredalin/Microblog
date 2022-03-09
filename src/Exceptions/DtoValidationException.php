dd67c3d37afea72170e044326736533eea5ec5bc
Hristo Hristov <koredalin@gmail.com>
2022-03-09 19:53:51 +0200

    Exceptions refactored by Coding Standards

--- src/Exceptions/DtoValidationException.php
+++ src/Exceptions/DtoValidationException.php
@@ -15,7 +15,7 @@ class DtoValidationException extends Exception
     public function __construct(string $message = "", int $code = 0, \Throwable $previous = null)
     {
         $code = ResponseStatuses::UNPROCESSABLE_ENTITY;
-        
+
         parent::__construct($message, $code, $previous);
     }
 }
